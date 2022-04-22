6b902fbccd6aee925e6d01cbc43133d6ce9f19ab
Edward Lalbem Lüderitz <edluder@gmail.com>
2022-04-22 01:44:37 -0300

    inserindo getter and setter do nascimento 3

--- src/programjavacode/codejava.java
+++ src/programjavacode/codejava.java
@@ -1,55 +1,18 @@
 package programjavacode;
 
-import java.util.Date;
-
 public class codejava {
+    static int maiorIdadeGlobal = 20;
 
-    private String nome;
+    public static void main (String[] args) {
+        int maiorIdade = 18;
 
-    private String sobrenome;
-
-    private int idade;
-
-    private Date dataNascimento;
-
-    private Date dataNascimento2;
-
-    public Date getDataNascimento2() {
-        return dataNascimento2;
+        metodo2();
     }
 
-    public void setDataNascimento2(Date dataNascimento2) {
-        this.dataNascimento2 = dataNascimento2;
+
+    public static void metodo2(){
+        System.out.println(maiorIdadeGlobal);
     }
 
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
 
-    public void setDataNascimento(Date dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
 }
