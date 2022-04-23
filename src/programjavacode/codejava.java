8347f204308efd30f5d56e1bab722fb40a13ad51
Edward Lalbem Lüderitz <edluder@gmail.com>
2022-04-22 21:37:17 -0300

    inserindo condicional de pj e pf para seleções

--- src/programjavacode/codejava.java
+++ src/programjavacode/codejava.java
@@ -3,7 +3,15 @@ package programjavacode;
 public class codejava {
 
     public static void main (String[] args) {
-            System.out.println("Pessoa Física");
+
+        int mediaAluno = 70;
+
+        if (mediaAluno >= 70){
+            System.out.println("Parabéns você foi aprovado");
+        } else {
+            System.out.println("Você foi reprovado!");
+        }
+
     }
 
 }
