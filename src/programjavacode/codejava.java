4274d37ebc0769ae233e59d716974753908308bf
Edward Lalbem Lüderitz <edluder@gmail.com>
2022-04-23 00:58:50 -0300

    Operador ternário adicionado ao código

--- src/programjavacode/codejava.java
+++ src/programjavacode/codejava.java
@@ -2,18 +2,13 @@ package programjavacode;
 
 public class codejava {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         int mediaAluno = 70;
-        String nome = "Edward";
 
-        if (mediaAluno >= 70 && nome.equals("Edward")){
-            System.out.println("Parabéns você foi aprovado");
-        } else if (mediaAluno < 70) {
-            System.out.println("Você foi reprovado!");
-        }else {
-            System.out.println("Não encontrado");
-        }
+        String media = mediaAluno >= 70 ? "Aluno Aprovado" : "Reprovado";
+
+        System.out.println(media);
 
     }
 
