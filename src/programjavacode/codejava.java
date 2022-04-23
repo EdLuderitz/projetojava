21a5ffd31186fbbcdd4ff272b58a033b01ae07a0
Edward Lalbem Lüderitz <edluder@gmail.com>
2022-04-22 21:40:50 -0300

    inserindo condicional adicional de nome

--- src/programjavacode/codejava.java
+++ src/programjavacode/codejava.java
@@ -5,11 +5,14 @@ public class codejava {
     public static void main (String[] args) {
 
         int mediaAluno = 70;
+        String nome = "Edward";
 
-        if (mediaAluno >= 70){
+        if (mediaAluno >= 70 && nome.equals("Edward")){
             System.out.println("Parabéns você foi aprovado");
-        } else {
+        } else if (mediaAluno < 70) {
             System.out.println("Você foi reprovado!");
+        }else {
+            System.out.println("Não encontrado");
         }
 
     }
