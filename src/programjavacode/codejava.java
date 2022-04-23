bd3e8a4f62ba8328a18766c7dd2c89897d82a659
Edward Lalbem Lüderitz <edluder@gmail.com>
2022-04-23 17:49:31 -0300

    Estrutura de repetição WHILE

--- src/programjavacode/codejava.java
+++ src/programjavacode/codejava.java
@@ -6,20 +6,12 @@ import java.util.Scanner;
 public class codejava {
 
     public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
-        int mediaAluno;
-        int mediaAluno2;
-        System.out.println("Qual o valor?");
-        mediaAluno = ler.nextInt();
-        System.out.println("Qual o valor?");
-        mediaAluno2 = ler.nextInt();
-
-        if (mediaAluno == mediaAluno2 ){
-            System.out.println("As notas são iguais");
-        } else {
-            System.out.println("As notas não são iguais");
+        int numero = 0;
+        
+        while (numero <= 10) {
+            System.out.println("O número atual é: " + numero);
+            numero++;
         }
-
     }
 
 }
