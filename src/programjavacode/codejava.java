2f4afaa78fe68abaf619e708c007f4cd3a80252b
Edward Lalbem Lüderitz <edluder@gmail.com>
2022-04-23 17:32:20 -0300

    operação com switch case da mediaaluno

--- src/programjavacode/codejava.java
+++ src/programjavacode/codejava.java
@@ -1,5 +1,6 @@
 package programjavacode;
 
+import java.awt.desktop.SystemSleepEvent;
 import java.util.Scanner;
 
 public class codejava {
@@ -7,16 +8,16 @@ public class codejava {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         int mediaAluno;
+        int mediaAluno2;
         System.out.println("Qual o valor?");
         mediaAluno = ler.nextInt();
+        System.out.println("Qual o valor?");
+        mediaAluno2 = ler.nextInt();
 
-        switch (mediaAluno) {
-            case 70:
-                System.out.println("Aluno Aprovado");
-                break;
-
-            default: System.out.println("Outro valor inserido: " + mediaAluno);
-            break;
+        if (mediaAluno == mediaAluno2 ){
+            System.out.println("As notas são iguais");
+        } else {
+            System.out.println("As notas não são iguais");
         }
 
     }
