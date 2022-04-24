31e36929ab17570bb8b7d82751a72fc8dbb301c6
Edward Lalbem Lüderitz <edluder@gmail.com>
2022-04-23 23:16:52 -0300

    inserção de mais código para a visualização e inserção de dados

--- src/programjavacode/codejava.java
+++ src/programjavacode/codejava.java
@@ -7,15 +7,18 @@ public class codejava {
 
     public static void main(String[] args) {
 
-        String numero = JOptionPane.showInputDialog("Qual o valor?");
+        String carros = JOptionPane.showInputDialog("Quantos carros?");
+        String pessoas = JOptionPane.showInputDialog("Quantas pessoas?");
 
-        double carroNumero = Double.parseDouble(numero);
+        double carroNumero = Double.parseDouble(carros);
+        double pessoaNumero = Double.parseDouble(pessoas);
 
-        int divisao = (int) (carroNumero / 2);
+        int divisao = (int) (carroNumero / pessoaNumero);
 
-        JOptionPane.showMessageDialog(null, divisao);
+        double resto = carroNumero % pessoaNumero;
 
-
+        JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao + " carros" +
+                " e sobrou " + resto + "carro");
     }
 
 }
